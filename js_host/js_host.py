markfinger/python-js-host

commit a215bba6f8e3609ec185dcba0356eb3a5e55e846

Reduced the startup time on Managed hosts.
The python process now shares the manager's config temporarily. Once the host has started or connected, it uses the real host's.

js_host/js_host.py
@@ -25,12 +25,12 @@ class JSHost(BaseServer):
             if 'config_file' in kwargs:
                 self.config_file = kwargs.pop('config_file')
             else:
+                # Reuse the manager's config so that we can avoid spinning up another process
+                # just to read the config file. When a managed host starts or connects, it
+                # overrides the config with the information returned from the manager
                 self.config_file = self.manager.get_path_to_config_file()
-
-            status = self.manager.request_host_status(self.get_path_to_config_file())
-            if status['started']:
-                self.status = json.loads(status['host']['output'])
-                self.logfile = status['host']['logfile']
+                self.status = copy.deepcopy(self.manager.get_status())
+                self.status['type'] = self.expected_type_name
 
         super(JSHost, self).__init__(*args, **kwargs)
 
@@ -69,6 +69,11 @@ class JSHost(BaseServer):
 
     def connect(self):
         if self.manager:
+            data = self.manager.request_host_status(self.get_path_to_config_file())
+            if data['started']:
+                self.status = json.loads(data['host']['output'])
+                self.logfile = data['host']['logfile']
+
             if not self.connection:
                 data = self.manager.open_connection_to_host(self.get_path_to_config_file())
                 self.connection = data['connection']

js_host/base_server.py
@@ -31,6 +31,8 @@ class BaseServer(object):
     version = None
     has_connected = False
 
+    has_validated_status = False
+
     def __init__(self, config_file=None, source_root=None, path_to_node=None):
         if not self.config_file:
             self.config_file = config_file or settings.CONFIG_FILE
@@ -64,29 +66,6 @@ class BaseServer(object):
 
         if not os.path.exists(self.get_path_to_config_file()):
             raise ConfigError('Config file {} does not exist'.format(self.get_path_to_config_file()))
-
-        version = self.get_version().split('.')
-        for i, number in enumerate(self.supported_version):
-            if version[i] != number:
-                raise ConfigError(
-                    'Version {} of the js-host JavaScript library is supported, the system reported {}'.format(
-                        '.'.join(self.supported_version),
-                        self.get_version(),
-                    )
-                )
-
-        type_name = self.get_type_name()
-        if type_name != self.expected_type_name:
-            raise ConfigError('Expected type {} but found {}'.format(self.expected_type_name, type_name))
-
-        # Validate the config file
-        config = self.get_config()
-        if config is None:
-            raise ConfigError('No config has been defined')
-        if 'address' not in config:
-            raise ConfigError('No address has been defined in {}'.format(config))
-        if 'port' not in config:
-            raise ConfigError('No port has been defined in {}'.format(config))
 
     def get_path_to_config_file(self):
         if os.path.isabs(self.config_file):
@@ -229,7 +208,35 @@ class BaseServer(object):
     def restart(self):
         raise NotImplementedError()
 
+    def validate_status(self):
+        version = self.get_version().split('.')
+        for i, number in enumerate(self.supported_version):
+            if version[i] != number:
+                raise ConfigError(
+                    'Version {} of the js-host JavaScript library is supported, the system reported {}'.format(
+                        '.'.join(self.supported_version),
+                        self.get_version(),
+                    )
+                )
+
+        type_name = self.get_type_name()
+        if type_name != self.expected_type_name:
+            raise ConfigError('Expected type {} but found {}'.format(self.expected_type_name, type_name))
+
+        # Validate the config file
+        config = self.get_config()
+        if config is None:
+            raise ConfigError('No config has been defined')
+        if 'address' not in config:
+            raise ConfigError('No address has been defined in {}'.format(config))
+        if 'port' not in config:
+            raise ConfigError('No port has been defined in {}'.format(config))
+
     def connect(self):
+        if not self.has_validated_status:
+            self.validate_status()
+            self.has_validated_status = True
+
         if not self.is_running():
             raise ConnectionError('Cannot connect to {}'.format(self.get_name()))
 

commit 0e21bd27d0790c3cf9f11f36c18056547633604a

Fixed a regression in a215bba which caused managed hosts to fail to reconnect.

js_host/js_host.py
@@ -25,12 +25,12 @@ class JSHost(BaseServer):
             if 'config_file' in kwargs:
                 self.config_file = kwargs.pop('config_file')
             else:
-                # Reuse the manager's config so that we can avoid spinning up another process
-                # just to read the config file. When a managed host starts or connects, it
-                # overrides the config with the information returned from the manager
                 self.config_file = self.manager.get_path_to_config_file()
-                self.status = copy.deepcopy(self.manager.get_status())
-                self.status['type'] = self.expected_type_name
+
+            data = self.manager.request_host_status(self.get_path_to_config_file())
+            if data['started']:
+                self.status = json.loads(data['host']['output'])
+                self.logfile = data['host']['logfile']
 
         super(JSHost, self).__init__(*args, **kwargs)
 
@@ -69,11 +69,6 @@ class JSHost(BaseServer):
 
     def connect(self):
         if self.manager:
-            data = self.manager.request_host_status(self.get_path_to_config_file())
-            if data['started']:
-                self.status = json.loads(data['host']['output'])
-                self.logfile = data['host']['logfile']
-
             if not self.connection:
                 data = self.manager.open_connection_to_host(self.get_path_to_config_file())
                 self.connection = data['connection']